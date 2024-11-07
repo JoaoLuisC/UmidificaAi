f3e7abaec16714d5f4cd7958601edf069c30dc59
Leonardo Paiva <71511623+leopaivap@users.noreply.github.com>
2024-11-07 09:41:17 -0300

    Add LCD

--- DHT_ACS_RELE/DHT_ACS_RELE.ino
+++ DHT_ACS_RELE/DHT_ACS_RELE.ino
@@ -1,8 +1,17 @@
+#include <LiquidCrystal_I2C.h>
 #include <DHT.h>
+#include <Wire.h>
+
 #define DHTPIN 2  // Pino digital 2 conectado ao DHT11
 #define DHTTYPE DHT11  // Tipo do sensor DHT
 
+#define col 16 
+#define lin  2 
+#define ende  0x27 
+
 DHT dht(DHTPIN, DHTTYPE);
+
+LiquidCrystal_I2C lcd(ende,col,lin);
 
 double maior_Valor = 0, valor_Corrente = 0, valor_Atual = 0, energia_gasta = 0;
 float tensao = 127, potencia = 0;
@@ -13,13 +22,21 @@ void setup() {
   dht.begin();
   pinMode(7, OUTPUT);
   pinMode(A0, INPUT);
+
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
 }
 
 void loop() {
   float umidade = dht.readHumidity();
-  
+  bool ligado = false;
+  String msg = "";
+  char strUmid[10], strEnergia[10];
+
   if (umidade <= 40) {
     digitalWrite(7, HIGH);  // Liga o umidificador
+    ligado = true;
 
     if (tempo_inicio == 0) {
         tempo_inicio = millis();  
@@ -30,6 +47,10 @@ void loop() {
 
     maior_Valor = valor_Atual * 0.004882812;
     valor_Corrente = (maior_Valor - 2.5) * 1000 / 66 / 1.41421356;  // Sensibilidade: 66mV/A para ACS712 30A | Conversão para RMS
+
+    if (valor_Corrente < 0) {
+      valor_Corrente = 0;  // Ignora corrente negativa
+    }
     potencia = valor_Corrente * tensao;
 
     // Calcular o tempo de operação em horas
@@ -42,11 +63,11 @@ void loop() {
     // Enquanto estiver operando, imprime a umidade na tela
     Serial.print("Umidade do ar no momento (%): ");
     Serial.println(umidade);
-
   } 
   else if (umidade >= 60) {
     digitalWrite(7, LOW);  // Desliga o umidificador
     tempo_inicio = 0;  
+    ligado = false;
   
     Serial.println("----- Relatório do Umidificador -----");
     Serial.print("Umidade do ar no momento (%): ");
@@ -59,7 +80,26 @@ void loop() {
   } 
   else {
     digitalWrite(7, LOW);  
-    Serial.print("Umidade do ar no momento (%): "); //trocar para printar no lcd o tanto que ta no momento e quando te potencia que ta gastando
-    Serial.println(umidade);
   }
+
+  dtostrf(umidade, 4, 2, strUmid);
+  msg = "UMIDADE: ";
+  msg += strUmid;
+  msg += "%";
+  
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(msg); 
+  lcd.setCursor(0,1);
+
+  if(ligado){
+    msg = "Umidificador ON";
+  } else {
+    dtostrf(energia_gasta, 4, 2, strEnergia);
+    msg = "TOTAL kWh: ";
+    msg += strEnergia;
+  }
+  lcd.print(msg);
+
+  delay(5000);
 }
